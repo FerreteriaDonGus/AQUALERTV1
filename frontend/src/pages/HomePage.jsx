f83da5723750d605c350eecbbca9167002e2f848
Carolos <cratoo46@gmail.com>
2025-10-14 08:17:53 -0600

    avance chiquito

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from "react";
 import Banner from "../components/Banner";
 import ForumsSection from "../components/ForumsSection";
 import CardNav from "../components/CardNav";
+import AnimatedList from "../components/AnimatedList";
+
+const anListitems = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7', 'Item 8', 'Item 9', 'Item 10']; 
+  
 
 const navItems = [
   {
@@ -66,6 +70,13 @@ function HomePage() {
             <p style={{textAlign: "center", marginTop:"2rem", color:"#555"}}>
                 {apiMessage && `Mensaje de la API: ${apiMessage}`}
             </p>
+            <AnimatedList
+              items={anListitems}
+              onItemSelect={(item, index) => console.log(item, index)}
+              showGradients={true}
+              enableArrowNavigation={true}
+              displayScrollbar={true}
+/>
         </div>
     );
 }
